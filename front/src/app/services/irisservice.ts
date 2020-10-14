4136b51ef8752fd65d0bd842ba1537ab43211f41
Vassil <vassil@progrepublic.ru>
2020-10-14 13:44:03 +1000

    Правки для prod

--- front/src/app/services/irisservice.ts
+++ front/src/app/services/irisservice.ts
@@ -8,25 +8,39 @@ import { environment } from 'src/environments/environment';
 export class IrisService {
     constructor(private http: HttpClient) {
         if (environment.production) {
-            this.serverhost = location.protocol + '//' +location.host;
+            this.serverhost = 'http://base.progrepublic.ru/csp/motion-calc/';
         } else {
             this.serverhost = 'http://localhost:57772/calc/';
         }
+        
+        if (environment.production) {
+            this.headers = {
+                headers: new HttpHeaders({
+                  'Content-Type':  'application/json',
+                  'Authorization': 'Basic ' + btoa('motioncalc:motioncalc9990')
+                })
+              };
+            
+        } else {
+            this.headers = { 
+                headers: new HttpHeaders({
+                    'Content-Type':  'application/json'
+                  })
+            };
+        }
     }
     serverhost: string;
-    serverlogin: string = '_SYSTEM';
-    serverpassword: string = 'SYS';
-
+    headers;
 
     get(term: string): any {
         const headers = new HttpHeaders()
         term = term.trim();
         const URL = this.serverhost+term;
-        return this.http.get(URL, { headers, responseType: 'json'});
+        return this.http.get(URL,this.headers);
     }
     post(data: any, term: string): any {
         const headers = new HttpHeaders()
         const URL = this.serverhost+ term;
-        return this.http.post(URL, data, { headers, responseType: 'json'});
+        return this.http.post(URL, data, this.headers);
     }
 }
